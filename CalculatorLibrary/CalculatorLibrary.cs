edc4da83d7ceb7442f76524e1d8cb38c1f3fa17d
ddienu <rockdann@hotmail.com>
2023-11-02 23:51:52 -0500

    Adding the writer value to save as Json.

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -30,22 +30,22 @@ namespace CalculatorLibrary
             {
                 case "a":
                     result = num1 + num2;
-                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Add");
                     break;
                 case "s":
                     result = num1 - num2;
-                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Subtract");
                     break;
                 case "m":
                     result = num1 * num2;
-                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Multiply");
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
                     if (num2 != 0)
                     {
                         result = num1 / num2;
-                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                        writer.WriteValue("Division");
                     }
                     break;
                 // Return text for an incorrect option entry.
